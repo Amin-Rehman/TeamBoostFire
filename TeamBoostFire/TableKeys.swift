1557755d3eab1377d3fc098e4330b0762ec34b96
Amin Rehman <amrehman@microsoft.com>
2019-04-29 20:38:05 +0200

    Adding TableKeys enum

---
+++ TeamBoostFire/TableKeys.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum TableKeys: String {
+    case MeetingState = "meeting_state"
+    case Suspended = "suspended"
+    case ActiveSpeaker = "active_speaker"
+    case Participants = "participants"
+    case MeetingParams = "meeting_params"
+    case MeetingTime = "meeting_time"
+    case Agenda = "agenda"
+    case MaxTalkTime = "max_talk_time"
+}
