027e6f1372433bbed0b291b75f4a86190a9ee0a4
Amin Rehman <amrehman@microsoft.com>
2019-05-09 23:16:32 +0200

    Adding self participant speaker time

--- TeamBoostFire/Controllers/Participant/ParticipantSelfSpeakerViewController.swift
+++ TeamBoostFire/Controllers/Participant/ParticipantSelfSpeakerViewController.swift
@@ -10,12 +10,46 @@ import UIKit
 
 class ParticipantSelfSpeakerViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    private var secondTickTimer: Timer?
+    private var secondTimerCount = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        secondTimerCount = 0
+        updateSpeakingTimerLabel()
+        startSecondTickerTimer()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        secondTimerCount = 0
+        stopSecondTickerTimer()
+    }
+
+    private func startSecondTickerTimer() {
+        secondTickTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self,
+                                               selector: #selector(secondTicked),
+                                               userInfo: nil, repeats: true)
+    }
+
+    private func stopSecondTickerTimer() {
+        secondTickTimer?.invalidate()
+        secondTickTimer = nil
     }
 
     @IBAction func doneTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
 
+
+    @objc func secondTicked() {
+        secondTimerCount = secondTimerCount + 1
+        updateSpeakingTimerLabel()
+    }
+
+    private func updateSpeakingTimerLabel() {
+        let timeElapsedString = secondTimerCount.minutesAndSecondsPrettyString()
+        timerLabel.text = timeElapsedString
+    }
 }
