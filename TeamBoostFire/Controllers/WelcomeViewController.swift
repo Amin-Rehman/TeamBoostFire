b318b621b4fad217320369388190a96572f9eea1
Amin Rehman <amrehman@microsoft.com>
2019-06-08 12:26:39 +0200

    Toggle the test mode

--- TeamBoostFire/Controllers/WelcomeViewController.swift
+++ TeamBoostFire/Controllers/WelcomeViewController.swift
@@ -28,5 +28,23 @@ class WelcomeViewController: UIViewController {
             appDelegate.testEnvironment = true
         }
 
+        if let popOverView = Bundle.main.loadNibNamed("PopoverView", owner: self, options: nil)?.first as? UIView {
+            popOverView.alpha = 0.0;
+            popOverView.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
+
+            self.view.addSubview(popOverView)
+            popOverView.center = self.view.center
+            UIView.animate(withDuration: 0.5, animations: {
+                popOverView.alpha = 1.0
+                popOverView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }) { _ in
+                // After the popover has appeared
+                UIView.animate(withDuration: 0.5, delay: 0.5, animations: {
+                    popOverView.alpha = 0.0
+                }, completion: { _ in
+                    popOverView.removeFromSuperview()
+                })
+            }
+        }
     }
 }
