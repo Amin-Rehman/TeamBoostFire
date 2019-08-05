aab94fb23c472cb0930dd8a12dd7eb0ba5f7b6a0
Amin Rehman <amrehman@microsoft.com>
2019-08-05 20:35:20 +0200

    Gamer console and increase build number

--- TeamBoostFire/Controllers/Participant/ParticipantGameControllerViewController.swift
+++ TeamBoostFire/Controllers/Participant/ParticipantGameControllerViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 
 class ParticipantGameControllerViewController: UIViewController {
 
+    @IBOutlet weak var agreeLabel: UILabel!
+    @IBOutlet weak var disagreeLabel: UILabel!
+
     override func viewDidLoad() {
+        agreeLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        disagreeLabel.transform = CGAffineTransform(rotationAngle: (-1) * CGFloat.pi / 2)
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.handleGesture(gesture:)))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.handleGesture(gesture:)))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.handleGesture(gesture:)))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.handleGesture(gesture:)))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
+        if gesture.direction == UISwipeGestureRecognizer.Direction.right {
+            print("Swipe Right")
+        }
+        else if gesture.direction == UISwipeGestureRecognizer.Direction.left {
+            print("Swipe Left")
+        }
+        else if gesture.direction == UISwipeGestureRecognizer.Direction.up {
+            print("Swipe Up")
+        }
+        else if gesture.direction == UISwipeGestureRecognizer.Direction.down {
+            print("Swipe Down")
+        }
     }
-    */
-
 }
