acc488d07de73ad5b132d8ab90e0346c7fa33fbe
Amin Rehman <amrehman@microsoft.com>
2019-08-18 18:56:02 +0200

    Adding lottie reactions

---
+++ TeamBoostFire/Controllers/Participant/ParticipantReactionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ParticipantReactionViewController.swift
+//  TeamBoostFire
+//
+//  Created by Amin Rehman on 18.08.19.
+//  Copyright © 2019 Amin Rehman. All rights reserved.
+//
+
+import UIKit
+
+class ParticipantReactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
