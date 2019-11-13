41b77a6e3caca387e8947c87c06c9640d7ab2447
Amin Rehman <amrehman@microsoft.com>
2019-11-13 11:00:32 +0100

    WIP: Writing some integration tests to make life a bit easier.

---
+++ TeamBoostFireTests/IntegrationTests/TeamBoostFirebaseIntegrationTests.swift
@@ -0,0 +1,73 @@
+//
+//  TeamBoostFirebaseIntegrationTests.swift
+//  TeamBoostFireTests
+//
+//  Created by Amin Rehman on 13.11.19.
+//  Copyright © 2019 Amin Rehman. All rights reserved.
+//
+
+import XCTest
+@testable import TeamBoostFire
+import Firebase
+import FirebaseDatabase
+
+
+class TeamBoostFirebaseIntegrationTests: XCTestCase {
+
+    override func setUp() {
+        let domain = Bundle.main.bundleIdentifier!
+        UserDefaults.standard.removePersistentDomain(forName: domain)
+        UserDefaults.standard.synchronize()
+
+        FirebaseApp.configure()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testMeetingGetsSetupOnTheHostSide() {
+        // Setup the meeting on the host side
+        let hostCoreServices = HostCoreServices.shared
+        let meetingParams = MeetingsParams(agenda: "Foo Agenda",
+                                           meetingTime: 4,
+                                           maxTalkTime: 10,
+                                           moderationMode: .AutoModerated)
+
+
+        hostCoreServices.setupCore(with: meetingParams)
+        let meetingIdentifier = hostCoreServices.meetingIdentifier!
+        
+        // Make (3) Participants join the meeting
+
+//        // Setup Participant 1
+//        let participant1 = Participant(id: "partipant1Id",
+//                                       name: "partipant1Name",
+//                                       speakerOrder: -1)
+//        let participantCoreServices1 = ParticipantCoreServices.shared
+//        participantCoreServices1.setupCore(with: participant1,
+//                                           meetingCode: meetingIdentifier)
+//
+//        // Setup Participant 2
+//        let participant2 = Participant(id: "partipant2Id",
+//                                       name: "partipant2Name",
+//                                       speakerOrder: -1)
+//
+//        let participantCoreServices2 = ParticipantCoreServices.shared
+//        participantCoreServices2.setupCore(with: participant2,
+//                                           meetingCode: meetingIdentifier)
+//
+//        // Setup Participant 3
+//        let participant3 = Participant(id: "partipant3Id",
+//                                       name: "partipant3Name",
+//                                       speakerOrder: -1)
+//
+//        let participantCoreServices3 = ParticipantCoreServices.shared
+//        participantCoreServices3.setupCore(with: participant3,
+//                                           meetingCode: meetingIdentifier)
+//
+//
+//        hostCoreServices.startMeeting()
+    }
+
+}
