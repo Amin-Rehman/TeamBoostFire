e91c9acda0f799bb71b8a5330ade5c9a977e371b
Amin Rehman <amrehman@microsoft.com>
2019-11-15 23:35:34 +0100

    commit testMeetingGetsSetupOnTheHostSideWithCorrectNumberOfParticipants

--- TeamBoostFireTests/IntegrationTests/TeamBoostFirebaseIntegrationTests.swift
+++ TeamBoostFireTests/IntegrationTests/TeamBoostFirebaseIntegrationTests.swift
@@ -21,7 +21,7 @@ class TeamBoostFirebaseIntegrationTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testMeetingGetsSetupOnTheHostSide() {
+    func testMeetingGetsSetupOnTheHostSideWithCorrectNumberOfParticipants() {
         // Setup the meeting on the host side
         let hostCoreServices = HostCoreServices.shared
         let meetingParams = MeetingsParams(agenda: "Foo Agenda",
