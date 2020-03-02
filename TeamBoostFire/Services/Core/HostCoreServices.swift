8d8d0b7fe1f7bcace000f0571f65ba89f2df9b7c
Amin-Rehman <aminurrehmankhan@hotmail.com>
2020-03-02 08:48:45 +0100

    Moving some bits about in HostCoreServices (#78)
    Co-authored-by: Amin Ur Rehman <amrehman@microsoft.com>

--- TeamBoostFire/Services/Core/HostCoreServices.swift
+++ TeamBoostFire/Services/Core/HostCoreServices.swift
@@ -17,6 +17,8 @@ class HostCoreServices: TeamBoostCore {
     private var firebaseReferenceHolder: ReferenceHolding?
     private var firebaseReferenceObserver: ReferenceObserving?
 
+    // MARK: - Public
+
     private init() {}
 
     public func setupCore(with params: MeetingsParams,
@@ -33,43 +35,6 @@ class HostCoreServices: TeamBoostCore {
         injectFakeParticipantsForTestModeIfNeeded()
     }
 
-    private func setupObservers() {
-        firebaseReferenceObserver?.observeParticipantListChanges(subscriber: { allParticipants in
-            self.allParticipants = allParticipants
-
-            DispatchQueue.main.async {
-                let name = Notification.Name(TeamBoostNotifications.participantListDidChange.rawValue)
-                NotificationCenter.default.post(name: name,
-                                                object: allParticipants)
-            }
-        })
-        firebaseReferenceObserver?.observeSpeakerOrderDidChange(subscriber: { speakerOrder in
-            self.speakerOrder = speakerOrder
-
-            DispatchQueue.main.async {
-                let name = Notification.Name(TeamBoostNotifications.speakerOrderDidChange.rawValue)
-                NotificationCenter.default.post(name: name,
-                                                object: speakerOrder)
-            }
-        })
-
-        firebaseReferenceObserver?.observeIAmDoneInterrupt(subscriber: {
-            DispatchQueue.main.async {
-                let name = Notification.Name(TeamBoostNotifications.participantIsDoneInterrupt.rawValue)
-                NotificationCenter.default.post(name: name,
-                                                object: nil)
-            }
-        })
-
-        firebaseReferenceObserver?.observeCallToSpeakerDidChange(subscriber: { callToSpeakerId in
-            DispatchQueue.main.async {
-                let name = Notification.Name(TeamBoostNotifications.callToSpeakerDidChange.rawValue)
-                NotificationCenter.default.post(name: name,
-                                                object: callToSpeakerId)
-            }
-        })
-    }
-    
     public func startMeeting() {
         var allParticipantIdentifiers = [String]()
         guard let allParticipantsGuarded = allParticipants else {
@@ -110,6 +75,47 @@ class HostCoreServices: TeamBoostCore {
     }
 }
 
+// MARK: - Private
+extension HostCoreServices {
+    private func setupObservers() {
+        firebaseReferenceObserver?.observeParticipantListChanges(subscriber: { allParticipants in
+            self.allParticipants = allParticipants
+
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.participantListDidChange.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: allParticipants)
+            }
+        })
+        firebaseReferenceObserver?.observeSpeakerOrderDidChange(subscriber: { speakerOrder in
+            self.speakerOrder = speakerOrder
+
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.speakerOrderDidChange.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: speakerOrder)
+            }
+        })
+
+        firebaseReferenceObserver?.observeIAmDoneInterrupt(subscriber: {
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.participantIsDoneInterrupt.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: nil)
+            }
+        })
+
+        firebaseReferenceObserver?.observeCallToSpeakerDidChange(subscriber: { callToSpeakerId in
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.callToSpeakerDidChange.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: callToSpeakerId)
+            }
+        })
+    }
+}
+
+// MARK: - Test mode
 extension HostCoreServices {
     public func injectFakeParticipantsForTestModeIfNeeded() {
         firebaseReferenceHolder?.testModeSetReferenceForNoParticipants()
