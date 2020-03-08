6417c0e000e1881c3446c62fe960d3ccd89baa98
Amin Rehman <amrehman@outlook.com>
2020-03-08 18:48:51 +0100

    Host Pusher Observing changes in the storage Pushing changes when needed

--- TeamBoostFire/CArch/Host/Data/Sync/FirebaseHostPusher.swift
+++ TeamBoostFire/CArch/Host/Data/Sync/FirebaseHostPusher.swift
@@ -8,6 +8,57 @@
 
 import Foundation
 
-struct FirebaseHostPusher {
+/**
+ Observes changes in the storage and pushes out the changes to firebase if there are local changes.
+ Local changes are determined if there is a time stamp.
+ */
+class FirebaseHostPusher: StorageChangeObserving {
+    var storage: PersistenceStorage
+    let firebaseReferenceHolder: ReferenceHolding
+    let meetingIdentifier: String
 
+    init(with storage: PersistenceStorage,
+         firebaseReferenceHolder: ReferenceHolding,
+         meetingIdentifier: String) {
+        self.storage = storage
+        self.firebaseReferenceHolder = firebaseReferenceHolder
+        self.meetingIdentifier = meetingIdentifier
+        self.storage.storageChangedObserver = self
+    }
+
+    func storageDidChange() {
+        pushSpeakerOrderIfNeeded()
+        pushModeratorHasControlIfNeeded()
+        pushMeetingStateIfNeeded()
+    }
+
+    private func pushSpeakerOrderIfNeeded() {
+        let speakerOrderValuePair = storage.speakerOrder(for: meetingIdentifier)
+        if speakerOrderValuePair.hasTimeStamp() {
+            firebaseReferenceHolder.setReferenceForSpeakerOrder(speakingOrder: speakerOrderValuePair.value)
+        }
+    }
+
+    private func pushModeratorHasControlIfNeeded() {
+        let moderatorHasControlValuePair = storage.moderatorHasControl(for: meetingIdentifier)
+        if moderatorHasControlValuePair.hasTimeStamp() {
+            firebaseReferenceHolder.setReferenceForModeratorHasControl(
+                controlState: moderatorHasControlValuePair.value)
+        }
+    }
+
+    private func pushMeetingStateIfNeeded() {
+        let meetingStateValuePair = storage.meetingState(for: meetingIdentifier)
+        if meetingStateValuePair.hasTimeStamp() {
+            switch meetingStateValuePair.value {
+            // TODO: Replace with enum
+            case "started":
+                firebaseReferenceHolder.setReferenceForMeetingStarted()
+            case "ended":
+                firebaseReferenceHolder.setReferenceForMeetingEnded()
+            default:
+                firebaseReferenceHolder.setReferenceForMeetingEnded()
+            }
+        }
+    }
 }
