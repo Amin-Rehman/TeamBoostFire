aaff39816265af58e2ecbb83d80dcbb645fdcb5c
Amin Ur Rehman <aminurrehmankhan@hotmail.com>
2020-03-15 14:11:48 +0100

    Firebase fetcher and increment build number for 1.2 (#91)
    Co-authored-by: Amin Rehman <amrehman@outlook.com>

--- TeamBoostFire/Northstar/Host/Data/Sync/FirebaseHostFetcher.swift
+++ TeamBoostFire/Northstar/Host/Data/Sync/FirebaseHostFetcher.swift
@@ -8,5 +8,54 @@
 
 import Foundation
 
-struct FirebaseHostFetcher {
+class FirebaseHostFetcher {
+    let storage: PersistenceStorage
+    let firebaseReferenceObserver: ReferenceObserving
+
+    init(with storage: PersistenceStorage,
+         firebaseReferenceObserver: ReferenceObserving) {
+        self.storage = storage
+        self.firebaseReferenceObserver = firebaseReferenceObserver
+    }
+
+    private func subscribeToReferenceChanges() {
+        // TODO: Remove TeamBoostNotifications and avoid leakage.
+        firebaseReferenceObserver.observeParticipantListChanges(subscriber: { allParticipants in
+
+            // TODO: Save in storage
+
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.participantListDidChange.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: allParticipants)
+            }
+        })
+        firebaseReferenceObserver.observeSpeakerOrderDidChange(subscriber: { speakerOrder in
+            // TODO: Save in storage
+
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.speakerOrderDidChange.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: speakerOrder)
+            }
+        })
+
+        firebaseReferenceObserver.observeIAmDoneInterrupt(subscriber: {
+            // TODO: Save in storage
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.participantIsDoneInterrupt.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: nil)
+            }
+        })
+
+        firebaseReferenceObserver.observeCallToSpeakerDidChange(subscriber: { callToSpeakerId in
+            // TODO: Save in storage
+            DispatchQueue.main.async {
+                let name = Notification.Name(TeamBoostNotifications.callToSpeakerDidChange.rawValue)
+                NotificationCenter.default.post(name: name,
+                                                object: callToSpeakerId)
+            }
+        })
+    }
 }
